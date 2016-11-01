520423a1ffb56b9a22430e0ed70220bd8184a345
Benjamin Bergstein <bennyjbergstein@gmail.com>
2016-11-01 13:26:20 -0700

    Lipstick.

--- lib/tracker/option_parser.rb
+++ lib/tracker/option_parser.rb
@@ -6,27 +6,27 @@ module Tracker
       ::OptionParser.new do |options|
         options.banner = <<-BANNER
 
-tracker --list OBJECT_TYPE [--format FORMAT_NAME]
-tracker --fetch OBJECT_TYPE [--id OBJECT_ID][-i]
+tracker --list OBJECT_TYPE (--format FORMAT_NAME)
+tracker --fetch OBJECT_TYPE (--id OBJECT_ID)(-i)
 
 BANNER
         
-        options.on '--list OBJECT_TYPE', 'one of: stories' do |object_type|
+        options.on '--list OBJECT_TYPE', 'one of: stories, projects' do |object_type|
           arguments[:method] = :list
-          arguments[:object_type] = object_type
-        end
-        
-        options.on '--id OBJECT_ID', 'OBJECT_ID for --fetch' do |object_id|
-          arguments[:object_id] = object_id
-        end
-        
-        options.on '--fetch OBJECT_TYPE', 'story' do |object_type|
-          arguments[:method] = :fetch
           arguments[:object_type] = object_type
         end
         
         options.on '--format FORMAT_NAME', 'none (default), json' do |format_name|
           arguments[:format_name] = format_name
+        end
+  
+        options.on '--fetch OBJECT_TYPE', 'story' do |object_type|
+          arguments[:method] = :fetch
+          arguments[:object_type] = object_type
+        end
+  
+        options.on '--id OBJECT_ID', 'OBJECT_ID for --fetch' do |object_id|
+          arguments[:object_id] = object_id
         end
         
         options.on '-i', 'interactive --fetch' do
