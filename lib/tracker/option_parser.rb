e82fed8986fde7e2d23bdb9bfd404940f2839168
Benjamin Bergstein <bennyjbergstein@gmail.com>
2016-11-16 02:01:29 -0500

    Improve help.

--- lib/tracker/option_parser.rb
+++ lib/tracker/option_parser.rb
@@ -6,12 +6,17 @@ module Tracker
       ::OptionParser.new do |options|
         options.banner = <<-BANNER
 
-tracker --list OBJECT_TYPE (--format FORMAT_NAME)
-tracker --fetch OBJECT_TYPE (--id OBJECT_ID)(-i)
+tracker --list OBJECT_TYPE (--format FORMAT_NAME) (--parameter key,value)
+tracker --fetch OBJECT_TYPE --id OBJECT_ID (--commit)
+                            -i (--parameter key,value) (--commit)
+tracker --create OBJECT_TYPE (-i) (--parameter key,value ...)
+tracker --destroy OBJECT_TYPE --id OBJECT_ID
+tracker --get URL
 
 BANNER
         
-        options.on '--list OBJECT_TYPE', 'one of: stories, projects, accounts' do |object_type|
+        options.separator "\n\e[37mLIST\e[0m"
+        options.on '--list OBJECT_TYPE', 'one of: accounts, memberships, projects, stories' do |object_type|
           arguments[:method] = :list
           arguments[:object_type] = object_type
         end
@@ -27,6 +32,8 @@ BANNER
           arguments[:format_name] = format_name
         end
   
+        options.separator "\n\e[37mFETCH\e[0m"
+  
         options.on '--fetch OBJECT_TYPE', 'story' do |object_type|
           arguments[:method] = :fetch
           arguments[:object_type] = object_type
@@ -40,20 +47,26 @@ BANNER
           arguments[:interactive] = true
         end
         
-        options.on '--commit', 'make a commit' do
+        options.on '--commit', 'make a commit (must be used with --fetch story)' do
           arguments[:commit] = true
         end
-        
+  
+        options.separator "\n\e[37mCREATE\e[0m"
+  
         options.on '--create OBJECT_TYPE', 'create a project or story' do |object_type|
           arguments[:method] = :create
           arguments[:object_type] = object_type
         end
+  
+        options.separator "\n\e[37mDESTROY\e[0m"
 
-        options.on '--destroy OBJECT_TYPE', 'create a project' do |object_type|
+        options.on '--destroy OBJECT_TYPE', 'destroy a project' do |object_type|
           arguments[:method] = :destroy
           arguments[:object_type] = object_type
         end
-        
+  
+        options.separator "\n\e[37mGET\e[0m"
+  
         options.on '--get URL', 'get a url endpoint' do |url|
           arguments[:method] = :request
           arguments[:request_method] = :get
@@ -67,3 +80,4 @@ BANNER
     end
   end
 end
+
